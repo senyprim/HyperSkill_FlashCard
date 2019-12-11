d825d4ea05df85b2bb13c8ba1da6e02e60f2e38d
senyprim <seny_ussur@mail.ru>
2019-12-11 10:32:45 +1000

    Stage 6

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -15,12 +15,31 @@ class Card{
     public final String term;
     public final String definition;
     int errorsCount=0;
-    public Card(String term,String definition){
+    public Card(String term,String definition,int errorsCount){
         this.term=term;
         this.definition=definition;
+        this.errorsCount=errorsCount;
+    }
+    public Card(String str){
+        Matcher matcher=pattern.matcher(str);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Wrong deserialize string");
+        }
+        matcher.reset();
+        matcher.find();
+        this.term=matcher.group(1);
+        this.definition=matcher.group(2);
+        this.errorsCount=Integer.parseInt(matcher.group(3));
     }
     public void incErrors(){
         errorsCount++;
+    }
+
+    public int getErrorsCount(){
+        return this.errorsCount;
+    }
+    public void clearErrorsCount(){
+        this.errorsCount=0;
     }
 
     @Override
@@ -45,36 +64,32 @@ class Card{
                 ", errorsCount=" + errorsCount +
                 '}';
     }
-    public Card(String str){
-        Matcher matcher=pattern.matcher(str);
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException("Wrong deserialize string");
-        }
-        matcher.reset();
-        matcher.find();
-        this.term=matcher.group(1);
-        this.definition=matcher.group(2);
-        this.errorsCount=Integer.parseInt(matcher.group(3));
-    }
 }
 class Cards{
+
 
     private Map<String,Card> cards=null;
     private Map<String,Card> indexDefinition=null;
 
-    public int size(){return cards.size();}
-    public void addCard(String term,String definition){
-        if (term==null) return;
-        if (isContainsTerm(term)){
-            throw new IllegalArgumentException(String.format("The card \"%s\" already exists.", term));
+
+    public void resetStat(){
+        for (Map.Entry<String,Card> entry:cards.entrySet()) {
+            Card card=entry.getValue();
+            card.clearErrorsCount();
+            entry.setValue(card);
         }
-        if (isContainsDefinition(definition)){
-            throw new IllegalArgumentException(String.format("The definition \"%s\" already exists.", definition));
-        }
-        Card card=new Card(term,definition);
-        cards.put(term,card);
-        indexDefinition.put(definition,card);
     }
+    public int size(){return cards.size();}
+
+    public void addCard(Card card){
+        if (card==null) return;
+        if (isContainsTerm(card.term)){
+            removeCard(card.term);
+        }
+        cards.put(card.term,card);
+        indexDefinition.put(card.definition,card);
+    }
+
     public Card getCardByTerm(String term){
         return cards.get(term);
     }
@@ -102,18 +117,36 @@ class Cards{
     public String getFirstTerm(){
         return cards.entrySet().iterator().next().getKey();
     }
+    public int getMaxErrorsCount(){
+        int max=0;
+        for (Map.Entry<String,Card> entry:cards.entrySet()) {
+            if (entry.getValue().getErrorsCount()>max){
+                max=entry.getValue().getErrorsCount();
+            }
+        }
+        return max;
+    }
+    public List<Card> getHardestCards(){
+        List<Card> result=new ArrayList<>();
+        int max=0;
+        for (Map.Entry<String,Card> entry:cards.entrySet()) {
+            if (entry.getValue().getErrorsCount()>max){
+                max=entry.getValue().getErrorsCount();
+                result.clear();
+            }
+            if(entry.getValue().getErrorsCount()==max){
+                result.add(entry.getValue());
+            }
+        }
+        return max==0?new ArrayList<Card>(): result;
+    }
 
     public int importCards(String fileName){
         try{
             List<String> lines=Files.readAllLines(Paths.get(fileName));
             int count=lines.size();
             for(int i=0;i<count;i++){
-                Card card=new Card(lines.get(i));
-                if (isContainsTerm(card.term)){
-                    removeCard(card.term);
-                }
-                cards.put(card.term,card);
-                indexDefinition.put(card.definition,card);
+                addCard(new Card(lines.get(i)));
             }
             return count;
         }
@@ -121,7 +154,6 @@ class Cards{
             throw new IllegalArgumentException(String.format("File not found"));
         }
     }
-
     public int exportCards(String fileName){
         try(FileWriter writer=new FileWriter(new File(fileName)))
         {
@@ -140,7 +172,6 @@ class Cards{
         return cards.size();
     }
 
-
     public Cards() {
         this.cards=new LinkedHashMap<>(16,0.75f,true);
         this.indexDefinition=new HashMap<>();
@@ -152,7 +183,7 @@ class CardsHelper{
     private final static  String INPUT_COMMAND="Input the action (add, remove, import, export, ask, exit):\n";
     private final static  String INPUT_TERM="The card:\n";
     private final static  String INPUT_DEFINITION="The definition of the card:\n";
-    private final static  String INPUT_SUCCESSFULL="The pair (\"%s\":\"%s\") has been added.\n";
+    private final static  String INPUT_SUCCESSFUL ="The pair (\"%s\":\"%s\") has been added.\n";
 
     private final static  String REMOVE_WRONG="Can't remove \"%s\": there is no such card.\n";
     private final static  String REMOVE_SUCCESSFUL="The card has been removed.\n";
@@ -171,8 +202,22 @@ class CardsHelper{
     private final static  String ANSWER_WRONG="Wrong answer. The correct one is \"%s\".\n";
     private final static  String ANSWER_WRONG2="Wrong answer. The correct one is \"%s\", you've just written the definition of \"%s\".\n";
 
+    private  final static String HARDEST_NONE="There are no cards with errors.\n";
+    private  final static String HARDEST_ONE="The hardest card is %s. You have %d errors answering it.\n";
+    private  final static String HARDEST_MANY="The hardest cards are %s. You have %d errors answering them.\n";
+
+    private final static  String RESET_STATISTIC="Card statistics has been reset.\n";
+    private final static  String SAVE_LOG="The log has been saved.\n";
+
+
     public Cards cards =null;
     public Scanner scanner=null;
+    private List<String> log=null;
+
+    private void printAndLog(String format,Object ...args){
+        System.out.printf(format,args);
+        log.add(String.format(format,args));
+    }
 
     public boolean execCommand(String command){
         boolean exit=false;
@@ -183,33 +228,39 @@ class CardsHelper{
             case "export":{exportCard();break;}
             case "ask":{askQuestions();break;}
             case "exit":{exit=true;break;}
-            //default:{throw new IllegalArgumentException("Wrong command");}
+            case "log":{saveLog();break;}
+            case "hardest card":{getHardestCards();break;}
+            case "reset stats":{resetStat();break;}
         }
         return exit;
     }
 
+
+    public String inputFromConsole(String inputString){
+        printAndLog(inputString);
+        String result=scanner.nextLine();
+        log.add(result+"\n");
+        return result;
+    }
+
     public void inputCommand(){
         while(true){
-            System.out.printf(INPUT_COMMAND);
-            String command=scanner.nextLine();
+            String command=inputFromConsole(INPUT_COMMAND);
             if (execCommand(command)) break;
-            //System.out.println();
         }
-        System.out.println("Bye bye!");
+        printAndLog("Bye bye!\n");
     }
 
     public String inputTerm(){
-        System.out.printf(INPUT_TERM);
-        String term=scanner.nextLine();
+        String term=inputFromConsole(INPUT_TERM);
         if (!cards.isContainsTerm(term)) return term;
-        System.out.printf(UN_UNIQUE_TERM,term);
+        printAndLog(UN_UNIQUE_TERM,term);
         return null;
     }
     public String inputDefinition(){
-        System.out.printf(INPUT_DEFINITION);
-        String definition=scanner.nextLine();
+        String definition=inputFromConsole(INPUT_DEFINITION);
         if (!cards.isContainsDefinition(definition)) return definition;
-        System.out.printf(UN_UNIQUE_DEFINITION,definition);
+        printAndLog(UN_UNIQUE_DEFINITION,definition);
         return null;
     }
     public void  inputCard(){
@@ -217,73 +268,105 @@ class CardsHelper{
         if (term==null) return ;
         String definition=inputDefinition();
         if(definition==null) return;
-        cards.addCard(term, definition);
-        System.out.printf(INPUT_SUCCESSFULL,term,definition);
+        cards.addCard(new Card(term,definition,0));
+        printAndLog(INPUT_SUCCESSFUL,term,definition);
     }
 
     public void removeCard(){
-        System.out.printf(INPUT_TERM);
-        String term=scanner.nextLine();
+        String term=inputFromConsole(INPUT_TERM);
         if (!cards.isContainsTerm(term)) {
-            System.out.printf(REMOVE_WRONG,term);
+            printAndLog(REMOVE_WRONG,term);
             return;
         }
         cards.removeCard(term);
-        System.out.printf(REMOVE_SUCCESSFUL);
+        printAndLog(REMOVE_SUCCESSFUL);
     }
 
     public void importCard(){
-        System.out.printf(FILE_INPUT);
-        String file=scanner.nextLine();
+        String file=inputFromConsole(FILE_INPUT);
         if (!new File(file).isFile()){
-            System.out.printf(FILE_WRONG);
+            printAndLog(FILE_WRONG);
             return;
         }
         int count=cards.importCards(file);
-        System.out.printf(FILE_LOAD_SUCCESSFUL,count);
+        printAndLog(FILE_LOAD_SUCCESSFUL,count);
     }
 
     public void exportCard(){
-        System.out.printf(FILE_INPUT);
-        String file=scanner.nextLine();
+        String file=inputFromConsole(FILE_INPUT);
         int count=cards.exportCards(file);
-        System.out.printf(FILE_SAVE_SUCCESSFUL,count);
+        printAndLog(FILE_SAVE_SUCCESSFUL,count);
     }
 
     public void askQuestion(){
         Card card=cards.getCardByTerm(cards.getFirstTerm());
-        System.out.printf(QUESTION,card.term);
-
-        String answer=scanner.nextLine();
+        String answer=inputFromConsole(String.format(QUESTION,card.term));
         Card otherCard=cards.getCardByDefinition(answer);
         if (Objects.equals(card,otherCard)){
-            System.out.printf(ANSWER_CORRECT);
+            printAndLog(ANSWER_CORRECT);
         }
         else if (otherCard!=null){
-            System.out.printf(ANSWER_WRONG2,card.definition,otherCard.term);
+            printAndLog(ANSWER_WRONG2,card.definition,otherCard.term);
+            card.incErrors();
         }
         else {
-            System.out.printf(ANSWER_WRONG,card.definition);
+            printAndLog(ANSWER_WRONG,card.definition);
+            card.incErrors();
         }
     }
 
     public void askQuestions(){
-        System.out.printf(QUESTIONS_COUNT);
-        int count=Integer.parseInt(scanner.nextLine());
+        int count=Integer.parseInt(inputFromConsole(QUESTIONS_COUNT));
         for(int i=0;i<count&&cards.size()>0;i++){
             askQuestion();
         }
     }
 
-    CardsHelper(Cards cards,Scanner scanner){
+    public void saveLog(){
+        String file=inputFromConsole(FILE_INPUT);
+        try(FileWriter writer=new FileWriter(new File(file))){
+            for (int i=0;i<log.size();i++){
+                writer.write(log.get(i));
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        printAndLog(SAVE_LOG);
+    }
+
+    public void getHardestCards(){
+        List<Card> list=cards.getHardestCards();
+        if (list.size()==0){
+            printAndLog(HARDEST_NONE);
+        }
+        else {
+            String hardest="";
+            for(int i=0;i<list.size();i++){
+                hardest+=String.format("\"%s\"",list.get(i).term);
+                if (i<list.size()-1){
+                    hardest+=", ";
+                }
+            }
+            printAndLog(list.size()==1?HARDEST_ONE:HARDEST_MANY,hardest,list.get(0).errorsCount);
+        }
+    }
+
+    public void resetStat(){
+        cards.resetStat();
+        printAndLog(RESET_STATISTIC);
+    }
+
+    CardsHelper(Cards cards,Scanner scanner,List<String> log){
         this.cards=cards;
         this.scanner=scanner;
+        this.log=log;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        CardsHelper helper=new CardsHelper(new Cards(),new Scanner(System.in));
+        CardsHelper helper=new CardsHelper(new Cards(),new Scanner(System.in),new ArrayList<>());
         helper.inputCommand();
     }
 }
